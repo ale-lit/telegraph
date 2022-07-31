6b150bca2802aee6a58ed628834b05c5335f058e
ale-lit <zeldor@ya.ru>
2022-07-31 19:20:01 +0300

    refact: edit remove function

--- index.php
+++ index.php
@@ -15,8 +15,14 @@ add('Заголовок 2', 'Текст 2', $textStorage);
 print_r($textStorage);
 
 function remove(int $postId, array &$textStorage): bool {
-    return isset($textStorage[$postId]);
+    if (isset($textStorage[$postId])) {
+        unset($textStorage[$postId]);
+        return true;
+    }
+    return false;
 }
 
 var_dump(remove(0, $textStorage));
 var_dump(remove(5, $textStorage));
+
+print_r($textStorage);
